32995b74292abe412810433ee8951e06a98c5326
Gus Garcia <guscastles@gmail.com>
2018-06-03 22:13:15 +1000

    Added CSV module

--- test/excompendium_test.exs
+++ test/excompendium_test.exs
@@ -2,7 +2,17 @@ defmodule ExcompendiumTest do
   use ExUnit.Case
   doctest Excompendium
 
+  defp iterate([], accum), do: accum
+
+  defp iterate([h|t], accum) do
+    next = elem(h, 1)
+    iterate(t, accum ++ [next])
+  end
+
   test "greets the world" do
+    raw_data = "./rsc/test.csv" |> File.stream! |> CSV.decode |> Enum.take(2)
+    contents = iterate(raw_data, [])
+    assert contents.first().first() == "ID"
     assert Excompendium.hello() == :world
   end
 end
