dcc418d044e8e0f3a1b4e7ffa84416cf6db43a07
Gus Garcia <guscastles@gmail.com>
2018-06-09 21:05:56 +1000

    Added Fibonacci module for training

--- test/fibonacci_test.exs
+++ test/fibonacci_test.exs
@@ -16,4 +16,14 @@ defmodule FibonacciTest do
   test "sequence of 3 elements" do
     assert Fibonacci.of(3) == [1, 1, 2]
   end
+
+  test "sequence of 4 to 20 elements" do
+    assert :end == 4..20 |> Enum.to_list |> _show_fibo()
+  end
+
+  defp _show_fibo([]), do: :end
+  defp _show_fibo([h|t]) do
+    Fibonacci.of(h) |> IO.inspect()
+    _show_fibo(t)
+  end
 end
