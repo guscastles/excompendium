4e002faa2270ada33a4f09a0bcc970d8ed369288
Gus Garcia <guscastles@gmail.com>
2018-06-09 21:31:19 +1000

    Refactored Fibonacci._of functions to be private

--- lib/fibonacci.ex
+++ lib/fibonacci.ex
@@ -2,6 +2,6 @@ defmodule Fibonacci do
   def of(0), do: []
   def of(n) when n < 3, do: [1] ++ of(n - 1)
   def of(n), do: _of(n - 2, of(2))
-  def _of(0, accum), do: accum
-  def _of(n, accum), do: _of(n - 1, accum ++ [accum |> Enum.reverse |> Enum.take(2) |> Enum.sum])
+  defp _of(0, accum), do: accum
+  defp _of(n, accum), do: _of(n - 1, accum ++ [accum |> Enum.reverse |> Enum.take(2) |> Enum.sum])
 end
