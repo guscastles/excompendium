0da21bb01fb3965f172c5e703dad5af11fcd8328
Gus Garcia <guscastles@gmail.com>
2018-06-09 21:36:15 +1000

    Some refactoring in Fibonacci module

--- lib/fibonacci.ex
+++ lib/fibonacci.ex
@@ -1,7 +1,6 @@
 defmodule Fibonacci do
   def of(0), do: []
-  def of(n) when n < 3, do: [1] ++ of(n - 1)
-  def of(n), do: _of(n - 2, of(2))
-  defp _of(0, accum), do: accum
-  defp _of(n, accum), do: _of(n - 1, accum ++ [accum |> Enum.reverse |> Enum.take(2) |> Enum.sum])
+  def of(n), do: _of(n, 1, [1])
+  defp _of(max, n, accum) when n >= max, do: accum
+  defp _of(max, n, accum), do: _of(max, n + 1, accum ++ [accum |> Enum.reverse |> Enum.take(2) |> Enum.sum])
 end
