db5ba2a01f369c7a4a495845de7ea3e33d359de0
Gus Garcia <guscastles@gmail.com>
2018-06-11 09:15:46 +1000

    Adding input handling without throwing exceptions

--- lib/list2.ex
+++ lib/list2.ex
@@ -2,12 +2,16 @@ defmodule List2 do
   @moduledoc ~S"""
   New list module, for training purposes.
   """
+  def index(list \\ [], element \\ :any_element)
+  def index(list, _) when not is_list(list), do: index()
   def index([], _), do: -1
-  def index(l, element), do: _index(l, element, 0)
-  defp _index([], _, _), do: -1
+  def index(list, element), do: _index(list, element, 0)
+  defp _index([], _, _), do: index([])
   defp _index([h|_], element, position) when h == element, do: position 
   defp _index([h|t], element, position) when h != element, do: _index(t, element, position + 1)
 
+  def reverse(list \\ [])
+  def reverse(list) when not is_list(list), do: reverse()
   def reverse([]), do: [] 
   @doc """
   This is being used to test the debugging features or Elixir
