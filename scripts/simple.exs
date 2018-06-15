47c3be56388021202ef19549fa6a2216c549bb76
Gustavo Castilhos Garcia <guscastles@gmail.com>
2018-06-15 11:10:32 +1000

    Updated test for tree handler. WIP

--- scripts/simple.exs
+++ scripts/simple.exs
@@ -1,1 +1,13 @@
-IO.puts "Hello, World!"
+defmodule TreeHandler do
+
+  def create_tree() do
+    import Tree
+    import TreeNode
+    root = %TreeNode{id: 1}
+    put_in(root, %{nodes: [%TreeNode{id: 2}]})
+    tree = %Tree{}
+    put_in(tree, root)
+  end
+
+end
+
--- test/tree_handler_test.exs
+++ test/tree_handler_test.exs
@@ -1,18 +1,29 @@
 defmodule TreeHandlerTest do
   use ExUnit.Case
-  import TreeHandler, only: [update_node: 3]
-  import List, only: [first: 1]
+  import List, only: [last: 1]
 
-  test "updates one node from the root" do
-    assert _create_tree().root.id == 1
-    new_root = update_node root, first(root.nodes), %TreeNode{id: 2}
-    assert new_root.id == 1
-    assert first(new_root.nodes).id == 2
+  test "creates a testing tree" do
+    tree = _create_tree()
+    assert tree.root.id == 0
   end
 
-  defp _create_tree() do
-    import Tree
-    import TreeNode
-    %Tree{root: %TreeNode{id: 1, nodes: [%TreeNode{id: 2}]}}
+  test "adds one node to the root" do
+    root = _create_tree().root
+    new_root = _add_node(root, 2)
+    assert last(new_root.nodes).id == 2
+  end
+
+  test "updates one of the root's nodes" do
+    root = _add_node(_create_tree().root, 2)
+    new_root = root.nodes |> update_in(&(&1 ++ [%TreeNode{id: 3}]))
+    assert last(new_root.nodes).id == 3
+  end
+
+  defp _add_node(node, new_id) do
+    node.nodes |> put_in([%TreeNode{id: new_id}])
+  end
+
+  defp _create_tree(id \\ 0) do
+    put_in(%Tree{}.root.id, id)
   end
 end
